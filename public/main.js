2b4d8c4e7a416d7c76891fb8eb695d305a9a42a7
Raul Yesid Rojas Ballesteros <131273447+YesidRojas06@users.noreply.github.com>
2023-08-31 12:56:05 -0500

    Primer commit

---
+++ public/main.js
@@ -0,0 +1,11 @@
+document.getElementById("generarTicket").addEventListener('click', function(){
+    let nuevaPest = window.open("ticketera.html")
+    nuevaPest.addEventListener("load", function (){
+        let contador = 0
+        nuevaPest.document.getElementById('generarTickets').addEventListener("click", function(){
+            contador++;
+            let text = nuevaPest.document.getElementById("numero")
+            text.textContent = "Ticket: "+contador
+        })
+    })
+})
